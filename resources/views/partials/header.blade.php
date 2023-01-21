3897d2b77ccc060ad0bcf14aea306dc9210623e1
Sofia-Citrini <sofiacitrini@gmail.com>
2023-01-21 11:40:09 +0100

    add search bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,25 @@
-{{-- inserisco header
-    che succesivamente inlcuderó nel mio layout --}}
 
-<header class="py-3">
-    <div class="container d-flex justify-content-between">
+<header class="pb-3">
+    <div class="bg-primary">
+        <div class="container d-flex justify-content-end gap-5">
+            <div class="text-white" style="font-size: .6rem">DC POWER&#8480; VISA&#174;</div>
+            <div class="text-white" style="font-size: .6rem">ADDITIONAL DC SITE</div>
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-between align-items-center pt-3">
         {{-- logo --}}
-        <div class="img-container">
+        <div class="img-container flex-shrink-0">
             <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="" class="img-fluid">
         </div>
 
         {{-- navbar --}}
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg flex-grow-1">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav gap-4">
                     <a class="nav-item nav-link" href="#">characters</a>
                     <a class="nav-item nav-link" href="#">comics</a>
                     <a class="nav-item nav-link" href="#">movies</a>
@@ -28,6 +33,14 @@
                 </div>
             </div>
         </nav>
+
+        {{-- searchbar --}}
+        <div class="flex-shrink-0">
+            <input class="search-bar" type="text" placeholder="Search">
+        </div>
+        {{-- <div class="input-group input-group-sm mb-3 flex-shrink-1">
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
+        </div> --}}
     </div>
 
     {{-- jumbotron --}}
